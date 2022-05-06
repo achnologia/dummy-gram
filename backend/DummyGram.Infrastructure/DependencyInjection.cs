eb13b779bffd49aed4a895ef2e18e7d15daeccf2
achnologia <lljen1uslllive@gmail.com>
2022-05-06 15:17:52 +0300

    configured EF core

---
+++ backend/DummyGram.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using DummyGram.Infrastructure.EFCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DummyGram.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("Default"),
+                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName));
+        });
+        
+        return services;
+    }
+}
