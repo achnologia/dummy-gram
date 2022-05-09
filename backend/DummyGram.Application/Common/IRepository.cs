9ac9ec034af95007289056ccf5ee71f3f15a48a2
achnologia <lljen1uslllive@gmail.com>
2022-05-09 18:12:29 +0300

    Added base Repository class

---
+++ backend/DummyGram.Application/Common/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using DummyGram.Domain.Entities;
+
+namespace DummyGram.Application.Common;
+
+public interface IRepository<TEntity> 
+    where TEntity: Entity
+{
+    public Task AddAsync(TEntity entity);
+    public Task<bool> UpdateAsync(TEntity entityToUpdate);
+    public Task<bool> DeleteAsync(int id);
+    public Task<TEntity> GetByIdAsync(int id);
+    public Task<TEntity> GetByIdNoTrackingAsync(int id);
+}
---
+++ backend/DummyGram.Application/Common/Repository.cs
@@ -0,0 +1,57 @@
+﻿using DummyGram.Domain.Entities;
+using DummyGram.Infrastructure.EFCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DummyGram.Application.Common;
+
+public class Repository<TEntity> : IRepository<TEntity> 
+    where TEntity: Entity
+{
+    protected readonly ApplicationDbContext _context;
+
+    public Repository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task AddAsync(TEntity entity)
+    {
+        await _context.Set<TEntity>().AddAsync(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<bool> UpdateAsync(TEntity entityToUpdate)
+    {
+        _context.Set<TEntity>().Update(entityToUpdate);
+        var updated = await _context.SaveChangesAsync();
+
+        return updated > 0;
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var entity = await GetByIdAsync(id);
+
+        if (entity is null)
+            return false;
+
+        _context.Set<TEntity>().Remove(entity);
+        var deleted = await _context.SaveChangesAsync();
+
+        return deleted > 0;
+    }
+    
+    public async Task<TEntity> GetByIdAsync(int id)
+    {
+        var entity = await _context.Set<TEntity>().SingleOrDefaultAsync(x => x.Id == id);
+
+        return entity;
+    }
+    
+    public async Task<TEntity> GetByIdNoTrackingAsync(int id)
+    {
+        var entity = await _context.Set<TEntity>().AsNoTracking().SingleOrDefaultAsync(x => x.Id == id);
+
+        return entity;
+    }
+}
