50d0d1f6f7fd03215b4d9003e42ceef52737b22e
achnologia <lljen1uslllive@gmail.com>
2022-05-11 14:58:48 +0300

    Added domain unit tests

---
+++ backend/DummyGram.Domain.UnitTests/Entities/AppUser_Tests.cs
@@ -0,0 +1,125 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using DummyGram.Domain.Entities;
+using Xunit;
+
+namespace DummyGram.Domain.UnitTests.Entities;
+
+public class AppUser_Tests
+{
+    [Fact]
+    public async Task UpdateDisplayName_ShouldUpdate()
+    {
+        // Arrange
+        var email = "test@email.com";
+        var newDisplayName = "New display name";
+        var user = new AppUser(email);
+
+        // Act
+        user.Update(newDisplayName);
+
+        // Assert
+        Assert.Equal(newDisplayName, user.DisplayName);
+    }
+
+    [Fact]
+    public async Task Subscribe_ShouldSubscribe()
+    {
+        // Arrange
+        var email = "test@email.com";
+        var subscriberEmail = "test1@email.com";
+        var user = new AppUser(email);
+        var userSubscriber = new AppUser(subscriberEmail);
+
+        // Act
+        user.Subscribe(userSubscriber);
+
+        // Assert
+        Assert.Equal(1, user.Subscribers.Count());
+        Assert.Equal(userSubscriber, user.Subscribers.First());
+    }
+    
+    [Fact]
+    public async Task Unsubscribe_ShouldUnsubscribe()
+    {
+        // Arrange
+        var email = "test@email.com";
+        var subscriberEmail = "test1@email.com";
+        var user = new AppUser(email);
+        var userSubscriber = new AppUser(subscriberEmail);
+
+        // Act
+        user.Subscribe(userSubscriber);
+        user.Unsubscribe(userSubscriber);
+
+        // Assert
+        Assert.Equal(0, user.Subscribers.Count());
+    }
+
+    [Fact]
+    public async Task HasSubscriber_ShouldReturnTrue()
+    {
+        // Arrange
+        var email = "test@email.com";
+        var subscriberEmail = "test1@email.com";
+        var user = new AppUser(email);
+        var userSubscriber = new AppUser(subscriberEmail);
+
+        // Act
+        user.Subscribe(userSubscriber);
+        var result = user.HasSubscriber(userSubscriber);
+
+        // Assert
+        Assert.True(result);
+    }
+    
+    [Fact]
+    public async Task DoesNotHaveSubscriber_ShouldReturnFalse()
+    {
+        // Arrange
+        var email = "test@email.com";
+        var subscriberEmail = "test1@email.com";
+        var user = new AppUser(email);
+        var userSubscriber = new AppUser(subscriberEmail);
+
+        // Act
+        var result = user.HasSubscriber(userSubscriber);
+
+        // Assert
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public async Task SavePost_ShouldSavePost()
+    {
+        // Arrange
+        var test = "test";
+        var email = "test@email.com";
+        var user = new AppUser(email);
+        var post = new Post(test, test, test);
+
+        // Act
+        user.SavePost(post);
+
+        // Assert
+        Assert.Equal(1, user.SavedPosts.Count());
+        Assert.Equal(post, user.SavedPosts.First());
+    }
+    
+    [Fact]
+    public async Task RemoveSavedPost_ShouldRemoveSavedPost()
+    {
+        // Arrange
+        var test = "test";
+        var email = "test@email.com";
+        var user = new AppUser(email);
+        var post = new Post(test, test, test);
+
+        // Act
+        user.SavePost(post);
+        user.RemoveSavedPost(post);
+
+        // Assert
+        Assert.Equal(0, user.SavedPosts.Count());
+    }
+}
---
+++ backend/DummyGram.Domain.UnitTests/Entities/Post_Tests.cs
@@ -0,0 +1,106 @@
+﻿using System.Threading.Tasks;
+using System.Linq;
+using DummyGram.Domain.Entities;
+using Xunit;
+
+namespace DummyGram.Domain.UnitTests.Entities;
+
+public class Post_Tests
+{
+    [Fact]
+    public async Task Update_ShouldUpdate()
+    {
+        // Arrange
+        var test = "test";
+        var newValue = "new value";
+        var post = new Post(test, test, test);
+
+        // Act
+        post.Update(newValue, newValue);
+
+        // Assert
+        Assert.Equal(newValue, post.ImageUrl);
+        Assert.Equal(newValue, post.Description);
+    }
+
+    [Fact]
+    public async Task AddComment_ShouldAddComment()
+    {
+        // Arrange
+        var test = "test";
+        var post = new Post(test, test, test);
+        var postComment = new PostComment(post.Id, "idUser", test);
+
+        // Act
+        post.AddComment(postComment);
+
+        // Assert
+        Assert.Equal(1, post.Comments.Count());
+        Assert.Equal(postComment, post.Comments.First());
+    }
+    
+    [Fact]
+    public async Task GetPostCommentById_ShouldReturnComment()
+    {
+        // Arrange
+        var test = "test";
+        var post = new Post(test, test, test);
+        var postComment = new PostComment(post.Id, "idUser", test);
+
+        // Act
+        post.AddComment(postComment);
+        var result = post.GetPostCommentById(0);
+
+        // Assert
+        Assert.Equal(postComment, result);
+    }
+    
+    [Fact]
+    public async Task RemoveComment_ShouldRemoveComment()
+    {
+        // Arrange
+        var test = "test";
+        var post = new Post(test, test, test);
+        var postComment = new PostComment(post.Id, "idUser", test);
+
+        // Act
+        post.AddComment(postComment);
+        post.RemoveComment(0);
+
+        // Assert
+        Assert.Equal(0, post.Comments.Count());
+    }
+    
+    [Fact]
+    public async Task Like_ShouldAddLike()
+    {
+        // Arrange
+        var test = "test";
+        var post = new Post(test, test, test);
+        var postLike = new PostLike(post.Id, "idUser");
+
+        // Act
+        post.Like(postLike);
+
+        // Assert
+        Assert.Equal(1, post.Likes.Count());
+        Assert.Equal(postLike, post.Likes.First());
+    }
+
+    [Fact]
+    public async Task RemoveLike_ShouldRemoveLike()
+    {
+        // Arrange
+        var test = "test";
+        var idUser = "idUser";
+        var post = new Post(test, test, test);
+        var postLike = new PostLike(post.Id, idUser);
+
+        // Act
+        post.Like(postLike);
+        post.RemoveLike(idUser);
+
+        // Assert
+        Assert.Equal(0, post.Likes.Count());
+    }
+}
