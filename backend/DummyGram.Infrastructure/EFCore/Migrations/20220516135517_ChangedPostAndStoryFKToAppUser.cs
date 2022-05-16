c5003afe7842d5c02048641788a83b03882c5326
m.kopytinlive@gmail.com <m.kopytinlive@gmail.com>
2022-05-16 16:56:57 +0300

    Changed Post and Story FK to AppUser instead of IdentityUser

---
+++ backend/DummyGram.Infrastructure/EFCore/Migrations/20220516135517_ChangedPostAndStoryFKToAppUser.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DummyGram.Infrastructure.EFCore.Migrations
+{
+    public partial class ChangedPostAndStoryFKToAppUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_AspNetUsers_IdUser",
+                table: "Posts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Stories_AspNetUsers_IdUser",
+                table: "Stories");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_AppUsers_IdUser",
+                table: "Posts",
+                column: "IdUser",
+                principalTable: "AppUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Stories_AppUsers_IdUser",
+                table: "Stories",
+                column: "IdUser",
+                principalTable: "AppUsers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_AppUsers_IdUser",
+                table: "Posts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Stories_AppUsers_IdUser",
+                table: "Stories");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_AspNetUsers_IdUser",
+                table: "Posts",
+                column: "IdUser",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Stories_AspNetUsers_IdUser",
+                table: "Stories",
+                column: "IdUser",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
